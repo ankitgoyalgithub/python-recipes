f3c5028dbc1eedb91b0ab708b54aaf101c30cc8c
ankitgoyalgithub <goyal.ankit.049@gmail.com>
2019-06-18 19:02:47 +0530

    queue code

---
+++ Data Structures/Queue_Impl/queue_impl.py
@@ -0,0 +1,41 @@
+class Queue(object):
+
+    def __init__(self, max_size=10):
+        self.max_size = max_size
+        self.front = 0
+        self.rear = 0
+        self.data = [0 for i in range(max_size)]
+    
+    def enqueue(self, data=None):
+        if self.rear == self.max_size - 1:
+            raise Exception("Queue Full")
+        
+        self.data[self.rear] = data
+        self.rear += 1
+
+    
+    def dequeue(self):
+        if self.front == self.rear:
+            raise Exception("Queue Empty")
+
+        value = self.data[self.front]
+        self.front += 1
+        return value
+
+if __name__ == '__main__':
+    q = Queue(10)
+    q.enqueue(1)
+    q.enqueue(2)
+    q.enqueue(3)
+    q.enqueue(4)
+    q.enqueue(5)
+    q.enqueue(6)
+    q.enqueue(7)
+    q.enqueue(8)
+    q.enqueue(9)
+    print("Ten Done")
+    q.enqueue(10)
+    q.enqueue(11)
+    
+
+        
