81205137744aa6d32864f2f0594a3be27aba855b
Ankit Goyal <goyal.ankit.049@gmail.com>
2019-11-03 19:33:39 +0530

    removed unused

--- Problems/Bunch.py
+++
@@ -1,8 +0,0 @@
-class Bunch(dict):
-    def __init__(self, *args, **kwargs):
-        super(Bunch, self).__init__(*args, **kwargs)
-        self.__dict__ = self
-
-if __name__ == '__main__':
-    x = Bunch(name="Jayne Cobb", position="Public Relations")
-    print(x.name)
--- Problems/edit_distance.py
+++
@@ -1,1 +0,0 @@
-# https://www.geeksforgeeks.org/edit-distance-dp-5/
